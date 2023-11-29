3b289e45b4a92ed834f08203b29a2833380f004c
milka_deluxe <milagalko1@gmail.com>
2023-11-29 12:47:35 +0200

    lab6 py files

---
+++ Lab6/IperfConnectionTest.py
@@ -0,0 +1,23 @@
+import subprocess
+import parser
+
+server_ip = '10.0.5.1'
+
+
+def client(server_ip):
+    try:
+        result = subprocess.run(['iperf3', '-c', server_ip], capture_output=True, text=True, check=True)
+        return result.stdout, None
+    except subprocess.CalledProcessError as e:
+        return None, f"Error: {e.stderr}"
+
+
+result, error = client(server_ip)
+
+if error:
+    print(error)
+else:
+    result_list = parser.parse(result)
+    for value in result_list:
+        if value['Transfer'] > 2 and value['Bitrate'] > 20:
+            print(value)
