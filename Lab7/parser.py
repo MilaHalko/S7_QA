94ec7001870b47e9b4fc9690550a9cc09cd12516
milka_deluxe <milagalko1@gmail.com>
2023-11-29 14:37:47 +0200

    lab7 py files

---
+++ Lab7/parser.py
@@ -0,0 +1,24 @@
+import re
+
+
+def parse(text):
+    regexp = r'\[\s*(\d+)\]\s+(\d+\.\d+-\d+\.\d+)\s+sec\s+(\d+(?:\.\d+)?)\s+(G|M)Bytes\s+(\d+(?:\.\d+)?)\s+(G|M)bits/sec\s+(\d+)\s+(\d+)\s+KBytes'
+    result_list = []
+    intervals = re.findall(regexp, text)
+
+    for interval in intervals:
+        id, interval, transfer, transfer_unit, bitrate, bitrate_unit, retr, cwnd = interval
+
+        transfer = float(transfer)
+        transfer *= 1000 if transfer_unit == 'G' else 1
+
+        bitrate = float(bitrate)
+        bitrate *= 1000 if bitrate_unit == 'G' else 1
+
+        retr = int(retr)
+        cwnd = int(cwnd)
+
+        result_dict = {'Interval': interval, 'Transfer': transfer, 'Bitrate': bitrate, 'Retr': retr, 'Cwnd': cwnd}
+        result_list.append(result_dict)
+
+    return result_list
