81fc03e6726be9f0ae05ed983a4281899a377fec
milka_deluxe <milagalko1@gmail.com>
2023-11-29 14:48:34 +0200

    error messages are changed + refactoring

--- Lab7/iperf_test.py
+++ Lab7/iperf_test.py
@@ -3,16 +3,12 @@ import parser
 
 class TestSuite():
     def test_iperf3_client_connection(self, client):
-        transfer_min_value = 2
-        bitrate_min_value = 20
-
-        transfer_criteria = lambda result: result['Transfer'] >= transfer_min_value
-        bitrate_criteria = lambda result: result['Bitrate'] >= bitrate_min_value
-
+        transfer_criteria = lambda result: result['Transfer'] >= 2
+        bitrate_criteria = lambda result: result['Bitrate'] >= 20
         output, error = client
 
         if error:
-            assert False, f"Error while connecting client: {error}"
+            assert False, f"Client connection error: {error}"
 
         results = parser.parse(output)
 
@@ -20,9 +16,9 @@ class TestSuite():
             assert False, f"Incorrect client output"
 
         if not any(transfer_criteria(result) for result in results):
-            assert False, f"Transfer criteria not met. All Transfer values < {transfer_min_value}"
+            assert False, f"No transfer criteria"
 
         if not any(bitrate_criteria(result) for result in results):
-            assert False, f"Bitrate criteria not met. All Bitrate values < {bitrate_min_value}"
+            assert False, f"No bitrate criteria"
 
         assert True
